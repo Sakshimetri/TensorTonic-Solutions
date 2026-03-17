8b274ab94d62eeaea936e50814a872d72ec77e80
Sakshimetri <147792035+Sakshimetri@users.noreply.github.com>
2026-03-17 20:13:02 +0530

    Add solution for global-avg-pooling

---
+++ global-avg-pooling/global-avg-pooling.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def global_avg_pool(x):
+    """
+    Compute global average pooling over spatial dims.
+    Supports (C,H,W) => (C,) and (N,C,H,W) => (N,C).
+    """
+    x = np.asarray(x, dtype=float)
+
+    # Case 1: (C, H, W)
+    if x.ndim == 3:
+        return np.mean(x, axis=(1, 2))
+
+    # Case 2: (N, C, H, W)
+    elif x.ndim == 4:
+        return np.mean(x, axis=(2, 3))
+
+    else:
+        raise ValueError("Input must be of shape (C,H,W) or (N,C,H,W)")
